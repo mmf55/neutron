45626df9c233d7f7fa7d805f5acd22a0542698de
Manuel Martins Fernandes <manuelfernandes55@ua.pt>
2016-04-08 17:02:00 +0100

    ...

--- neutron/plugins/ml2/extnet_plugin.py
+++ neutron/plugins/ml2/extnet_plugin.py
@@ -8,6 +8,20 @@ LOG = logging.getLogger(__name__)
 
 class ExtNetworkML2(plugin.Ml2Plugin,
                     ext_network_db_mixin.ExtNetworkDBMixin):
+    # List of all the extensions supported by the plugin extension
+    extensions_supported = ['extnode',
+                            'extsegment',
+                            'extlink',
+                            'extinterface'
+                            ]
+
+    def __init__(self):
+        super(ExtNetworkML2, self).__init__()
+
+    def supported_extension_aliases(self):
+        for extension in self.extensions_supported:
+            self._supported_extension_aliases.append(extension)
+        super(ExtNetworkML2, self).supported_extension_aliases()
 
     def create_extnode(self, context, ext_node):
         LOG.debug("I got here!!!!")
