3e4293e1ecb444a666a8a9d12c40c90e8c6d23e0
Manuel Martins Fernandes <manuelfernandes55@ua.pt>
2016-04-21 16:12:05 +0100

    ...

--- neutron/db/extnet_db.py
+++ neutron/db/extnet_db.py
@@ -5,17 +5,17 @@ from sqlalchemy import orm
 
 
 class ExtInterface(model_base.BASEV2, models_v2.HasId, models_v2.HasTenant):
-    __tablename__ = "extinterface"
+    __tablename__ = "extinterfaces"
 
     network_id = sa.Column(sa.String(36),
                            sa.ForeignKey('networks.id', ondelete='CASCADE'))
-    extnodeint_id = sa.Column(sa.String(36), sa.ForeignKey("extnodeint.id",
+    extnodeint_id = sa.Column(sa.String(36), sa.ForeignKey("extnodeints.id",
                                                      ondelete="CASCADE"))
     extnodeint = orm.relationship("ExtNodeInt", back_populates="extinterface")
 
 
 class ExtNode(model_base.BASEV2, models_v2.HasId):
-    __tablename__ = "extnode"
+    __tablename__ = "extnodes"
 
     # type here can be e.g. router, switch, virtual switch.
     name = sa.Column(sa.String(36))
@@ -23,20 +23,20 @@ class ExtNode(model_base.BASEV2, models_v2.HasId):
 
 
 class ExtNodeInt(model_base.BASEV2, models_v2.HasId):
-    __tablename__ = "extnodeint"
+    __tablename__ = "extnodeints"
 
     name = sa.Column(sa.String(36))
     # The type here can be e.g. SSID, port.
     type = sa.Column(sa.String(36))
-    extnode_id = sa.Column(sa.String(36), sa.ForeignKey("extnode.id",
+    extnode_id = sa.Column(sa.String(36), sa.ForeignKey("extnodes.id",
                                                      ondelete="CASCADE"))
-    extsegment_id = sa.Column(sa.String(36), sa.ForeignKey("extsegment.id",
+    extsegment_id = sa.Column(sa.String(36), sa.ForeignKey("extsegments.id",
                                                         ondelete="CASCADE"))
     extinterface = orm.relationship("ExtInterface", uselist=False, back_populates="extnodeint")
 
 
 class ExtSegment(model_base.BASEV2, models_v2.HasId):
-    __tablename__ = "extsegment"
+    __tablename__ = "extsegments"
 
     name = sa.column(sa.String(36))
     # Types supported can be e.g. VLAN, GRE, VXLAN.
@@ -45,26 +45,26 @@ class ExtSegment(model_base.BASEV2, models_v2.HasId):
 
 
 class ExtLink(model_base.BASEV2, models_v2.HasId):
-    __tablename__ = "extlink"
+    __tablename__ = "extlinks"
 
     # Tells the type of the links created e.g. VLAN, GRE, VXLAN
     type = sa.column(sa.String(36))
     network_id = sa.Column(sa.String(36),
                            sa.ForeignKey('networks.id', ondelete='CASCADE'))
     overlay_id = sa.Column(sa.String(36))
-    extsegment_id = sa.Column(sa.String(36), sa.ForeignKey("extsegment.id",
+    extsegment_id = sa.Column(sa.String(36), sa.ForeignKey("extsegments.id",
                                                         ondelete="CASCADE"))
 
 
 class ExtConnection(model_base.BASEV2, models_v2.HasId):
-    __tablename__ = "extconnection"
+    __tablename__ = "extconnections"
 
     # Connection types can be e.g. normal or trunk.
     type = sa.Column(sa.String(36))
 
-    extnodeint1_id = sa.Column(sa.String(36), sa.ForeignKey("extnodeint.id",
+    extnodeint1_id = sa.Column(sa.String(36), sa.ForeignKey("extnodeints.id",
                                                       ondelete="CASCADE"))
-    extnodeint2_id = sa.Column(sa.String(36), sa.ForeignKey("extnodeint.id",
+    extnodeint2_id = sa.Column(sa.String(36), sa.ForeignKey("extnodeints.id",
                                                       ondelete="CASCADE"))
-    extlink_id = sa.Column(sa.String(36), sa.ForeignKey("extlink.id",
+    extlink_id = sa.Column(sa.String(36), sa.ForeignKey("extlinks.id",
                                                      ondelete="CASCADE"))
