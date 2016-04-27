9b08d72f9d29755761fecedbf143ce70bc0b325e
Manuel Martins Fernandes <manuelfernandes55@ua.pt>
2016-04-27 23:33:19 +0100

    ...

--- neutron/db/extnet_db_query.py
+++ neutron/db/extnet_db_query.py
@@ -1,9 +1,4 @@
-from neutron.db import models_v2
-from neutron.plugins.ml2 import driver_api as api
-
-from neutron_lib import exceptions
 import sqlalchemy as sa
-from sqlalchemy.orm import exc
 
 
 class ExtNetworkCommonDbMixin(object):
@@ -31,24 +26,14 @@ class ExtNetworkCommonDbMixin(object):
             query = query.filter(query_filter)
         return query
 
-    def _get_collection_query(self, context, model, filters=None,
-                              sorts=None, limit=None, marker_obj=None,
-                              page_reverse=False):
+    def _get_collection_query(self, context, model, filters=None):
         """Get collection query for the models."""
         collection = self._model_query(context, model)
         collection = self._apply_filters_to_query(collection, model, filters)
         return collection
 
     def _get_collection(self, context, model, dict_func, filters=None,
-                        fields=None, sorts=None, limit=None, marker_obj=None,
-                        page_reverse=False):
+                        fields=None):
         """Get collection object based on query for resources."""
-        query = self._get_collection_query(context, model, filters=filters,
-                                           sorts=sorts,
-                                           limit=limit,
-                                           marker_obj=marker_obj,
-                                           page_reverse=page_reverse)
-        items = [dict_func(c, fields) for c in query]
-        if limit and page_reverse:
-            items.reverse()
-        return items
+        query = self._get_collection_query(context, model, filters=filters)
+        return [dict_func(c, fields) for c in query]
