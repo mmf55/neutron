206ccf2a12a8c09f565e880167de846e4224c18c
mfernandes <manuelfernandes55@ua.pt>
2016-04-27 14:10:16 +0100

    ...

--- neutron/db/extnet_db_mixin.py
+++ neutron/db/extnet_db_mixin.py
@@ -349,8 +349,8 @@ class ExtNetworkDBMixin(extnode.ExtNodePluginInterface,
                 for connection in connections:
                     connection_db = models.ExtConnection(
                         type=connection['type'],
-                        extnodeint1_id=connection['extnodeint1_id'],
-                        extnodeint2_id=connection['extnodeint2_id'],
+                        extnodeint1_id=connection['extnodeint1'],
+                        extnodeint2_id=connection['extnodeint2'],
                         extlink_id=link_db.id)
                     context.session.add(connection_db)
                     list_con_db.append(connection_db)
