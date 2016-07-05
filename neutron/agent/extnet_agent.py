8e490393e19bda294793d515e693cbee431987ca
mfernandes <manuelfernandes55@ua.pt>
2016-07-05 19:09:42 +0100

    ...

--- neutron/agent/extnet_agent.py
+++ neutron/agent/extnet_agent.py
@@ -1,7 +1,9 @@
+import sys
 
 from oslo_config import cfg
 from oslo_service import service
 
+from neutron.common import config as neutron_config
 from neutron.agent.extnet import config as extnet_config
 from neutron.agent.linux import external_process
 from neutron.agent.common import config
@@ -9,15 +11,16 @@ from neutron.common import topics
 from neutron import service as neutron_service
 
 
-def main(manager='neutron.agent.extnet.agent.ExtNetAgent'):
-
+def main():
+    neutron_config.init(sys.argv[1:])
     cfg.CONF.register_opts(external_process.OPTS)
     cfg.CONF.register_opts(extnet_config.OPTS)
     config.register_agent_state_opts_helper(cfg.CONF)
+    config.register_root_helper(cfg.CONF)
     config.setup_logging()
     server = neutron_service.Service.create(
         binary='neutron-extnet-agent',
         topic=topics.EXTNET_AGENT,
         report_interval=cfg.CONF.AGENT.report_interval,
-        manager=manager)
+        manager='neutron.agent.extnet.agent.ExtNetAgent')
     service.launch(cfg.CONF, server).wait()
