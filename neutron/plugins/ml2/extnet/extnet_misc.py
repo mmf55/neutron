30736c9670a231628d9b8083385cbc416135c877
mfernandes <manuelfernandes55@ua.pt>
2016-07-06 16:32:42 +0100

    ...

--- neutron/plugins/ml2/extnet/extnet_misc.py
+++ neutron/plugins/ml2/extnet/extnet_misc.py
@@ -128,9 +128,9 @@ class ExtNetDeviceCtrlManager(dev_ctrl_mgr.ExtNetDeviceControllerManager):
         target = oslo_messaging.Target(topic=topic, version='1.0')
         client = n_rpc.get_client(target)
         cctxt = client.prepare(topic=topic_create_extlink,
-                               fanout=True,
+                               fanout=False,
                                timeout=5)
-        return cctxt.cast(context,
+        return cctxt.call(context,
                           'deploy_link',
                           network_type=network_type,
                           segmentation_id=segmentation_id,
@@ -147,10 +147,10 @@ class ExtNetDeviceCtrlManager(dev_ctrl_mgr.ExtNetDeviceControllerManager):
         target = oslo_messaging.Target(topic=topic, version='1.0')
         client = n_rpc.get_client(target)
         cctxt = client.prepare(topic=topic_create_extport,
-                               fanout=True,
+                               fanout=False,
                                timeout=5)
 
-        return cctxt.cast(context,
+        return cctxt.call(context,
                           'deploy_port',
                           segmentation_id=segmentation_id,
                           interface=interface,
