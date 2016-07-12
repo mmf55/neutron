6658092361e090f73d63d54ae1433a387b2d1e9d
Manuel Martins Fernandes <manuelfernandes55@ua.pt>
2016-07-12 17:24:35 +0100

    ...

--- neutron/plugins/ml2/plugin.py
+++ neutron/plugins/ml2/plugin.py
@@ -1126,6 +1126,7 @@ class Ml2Plugin(db_base_plugin_v2.NeutronDbPluginV2,
         registry.notify(resources.PORT, events.AFTER_CREATE, self, **kwargs)
 
         ext_port = port.get('external_port')
+        LOG.debug(port)
         if ext_port:
             LOG.debug("I'm here!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!")
             self.create_extport(context, port)
