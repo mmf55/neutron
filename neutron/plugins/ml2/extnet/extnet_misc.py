4b36a7e420afea5520a3c77e2ec549d443ebb1a2
Manuel Martins Fernandes <manuelfernandes55@ua.pt>
2016-07-20 19:19:58 +0100

    ...

--- neutron/plugins/ml2/extnet/extnet_misc.py
+++ neutron/plugins/ml2/extnet/extnet_misc.py
@@ -186,8 +186,8 @@ class ExtNetControllerMixin(extnet_db_mixin.ExtNetworkDBMixin,
 
     def delete_extport(self, resource, event, plugin, **kwargs):
         context = kwargs['context']
-        port = kwargs['port']
-        port = self.get_extport(context, port.get('port_id'))
+        port_id = kwargs['port_id']
+        port = self.get_extport(context, port_id)
         if port:
             interface = self.get_extinterface(context, port.get('extinterface_id'))
             if self.undeploy_port(interface,
