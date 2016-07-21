b83705a127dfc757215dbfdf306baa14eb130e05
Manuel Martins Fernandes <manuelfernandes55@ua.pt>
2016-07-21 22:44:10 +0100

    ...

--- neutron/agent/common/ovs_lib.py
+++ neutron/agent/common/ovs_lib.py
@@ -390,7 +390,7 @@ class OVSBridge(BaseOVS):
 
     def delete_external_tunnel_port(self, port_name,
                                     lvid=None):
-        LOG.debug('here!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!11')
+        LOG.debug('port_name')
         self.delete_port(port_name)
 
     def add_patch_port(self, local_name, remote_name):
