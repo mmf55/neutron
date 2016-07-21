1325f165b9332aab2f830b82cf458e1fa67b8582
Manuel Martins Fernandes <manuelfernandes55@ua.pt>
2016-07-21 12:33:45 +0100

    ...

--- neutron/plugins/ml2/extnet/extnet_misc.py
+++ neutron/plugins/ml2/extnet/extnet_misc.py
@@ -181,6 +181,7 @@ class ExtNetControllerMixin(extnet_db_mixin.ExtNetworkDBMixin,
         elif interface.get('type') == 'l3':
             ext_port['segmentation_id'] = None
 
+        LOG.debug(interface_extports[0].id)
         if not interface_extports:
             LOG.debug(ext_port)
             if self.deploy_port(interface,
