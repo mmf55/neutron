26d81f497eb58b54cbb0c7dd7819842e04ddfee5
Manuel Martins Fernandes <manuelfernandes55@ua.pt>
2016-07-21 14:22:15 +0100

    ...

--- neutron/plugins/ml2/plugin.py
+++ neutron/plugins/ml2/plugin.py
@@ -1125,9 +1125,9 @@ class Ml2Plugin(db_base_plugin_v2.NeutronDbPluginV2,
         kwargs = {'context': context, 'port': result}
         registry.notify(resources.PORT, events.AFTER_CREATE, self, **kwargs)
 
-        ext_port = port['port'].get('external_port')
-        if ext_port:
-            self.create_extport(context, port)
+        # ext_port = port['port'].get('external_port')
+        # if ext_port:
+        #     self.create_extport(context, port)
 
         try:
             self.mechanism_manager.create_port_postcommit(mech_context)
