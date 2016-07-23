a126ba518980730f50d8f6d2a2362df985cbed05
Manuel Martins Fernandes <manuelfernandes55@ua.pt>
2016-07-23 16:24:09 +0100

    ...

--- neutron/plugins/ml2/extensions/extport.py
+++ neutron/plugins/ml2/extensions/extport.py
@@ -32,8 +32,8 @@ class ExtPortExtensionDriver(api.ExtensionDriver,
     def extend_port_dict(self, session, base_model, result):
         extport_db = session.query(models.ExtPort).filter_by(id=result.get('id')).first()
         LOG.debug(extport_db)
-        if extport_db:
-            result[extport.EXT_INTERFACE_ID] = extport_db.interface_id
-        elif result.get(extport.EXT_INTERFACE_ID) is None:
+        # if extport_db:
+        #     result[extport.EXT_INTERFACE_ID] = extport_db.interface_id
+        if result.get(extport.EXT_INTERFACE_ID) is None:
             result[extport.EXT_INTERFACE_ID] = (extport.EXTENDED_ATTRIBUTES_2_0['ports']
                                                 [extport.EXT_INTERFACE_ID]['default'])
