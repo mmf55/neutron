9e733dd639d17ed7368a685e2672c9ec5592f8de
mfernandes <manuelfernandes55@ua.pt>
2016-08-04 12:21:21 +0100

    ...

--- neutron/plugins/ml2/extnet/extnet_misc.py
+++ neutron/plugins/ml2/extnet/extnet_misc.py
@@ -223,21 +223,22 @@ class ExtNetControllerMixin(extnet_db_mixin.ExtNetworkDBMixin,
 
         if ext_port:
             interface = self.get_extinterface(context, ext_port.get('extinterface_id'))
-            node = self.get_extnode(context, interface.get('extnode_id'))
+            if interface:
+                node = self.get_extnode(context, interface.get('extnode_id'))
 
-            interface_extports = self._extinterface_has_extports(context, interface.get('id'))
-            extport_db = context.session.query(models.ExtPort).filter_by(id=port_id)
-            for link in extport_db.extlinks:
-                if len(link.extports) == 1:
-                    self.delete_extlink(context, link.id)
+                interface_extports = self._extinterface_has_extports(context, interface.get('id'))
+                extport_db = context.session.query(models.ExtPort).filter_by(id=port_id)
+                for link in extport_db.extlinks:
+                    if len(link.extports) == 1:
+                        self.delete_extlink(context, link.id)
 
-            if len(interface_extports) == 1:
-                if self.undeploy_port(interface,
-                                      node,
-                                      ext_port.get('segmentation_id'),
-                                      vnetwork=port.get('network_id'),
-                                      context=context) != const.OK:
-                    raise extnet_exceptions.ExtPortErrorApplyingConfigs()
+                if len(interface_extports) == 1:
+                    if self.undeploy_port(interface,
+                                          node,
+                                          ext_port.get('segmentation_id'),
+                                          vnetwork=port.get('network_id'),
+                                          context=context) != const.OK:
+                        raise extnet_exceptions.ExtPortErrorApplyingConfigs()
 
     # ------------------------------------ Auxiliary functions ---------------------------------------
 
