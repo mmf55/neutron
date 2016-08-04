970b9ce49077b63225155a04e08930ba60ecfc2a
mfernandes <manuelfernandes55@ua.pt>
2016-08-04 17:40:52 +0100

    ...

--- neutron/plugins/ml2/extnet/extnet_misc.py
+++ neutron/plugins/ml2/extnet/extnet_misc.py
@@ -187,7 +187,6 @@ class ExtNetControllerMixin(extnet_db_mixin.ExtNetworkDBMixin,
                 if not segmentation_id:
                     raise extnet_exceptions.ExtLinkSegmentationIdNotAvailable()
                 else:
-                    LOG.debug(port.get('id'))
                     self.set_seg_id_extport(context, port.get('id'), segmentation_id)
         else:
             LOG.debug("Creating links for the new port...")
