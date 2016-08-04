1ee73e27e35e8a4071f38db3b9e90c83e8addb8a
mfernandes <manuelfernandes55@ua.pt>
2016-08-04 10:58:43 +0100

    ...

--- neutron/plugins/ml2/extensions/extport.py
+++ neutron/plugins/ml2/extensions/extport.py
@@ -22,14 +22,17 @@ class ExtPortExtensionDriver(api.ExtensionDriver,
         return self._supported_extension_alias
 
     def process_create_port(self, plugin_context, data, result):
-        if data[extport.EXT_INTERFACE_ID]:
+        if data[extport.EXT_NODE_NAME]:
             self._process_create_port(plugin_context, data, result)
 
     def process_update_port(self, plugin_context, data, result):
-        if extport.EXT_INTERFACE_ID in data:
+        if extport.EXT_NODE_NAME in data:
             self._process_update_port(plugin_context, data, result)
 
     def extend_port_dict(self, session, base_model, result):
-        if result.get(extport.EXT_INTERFACE_ID) is None:
-            result[extport.EXT_INTERFACE_ID] = (extport.EXTENDED_ATTRIBUTES_2_0['ports']
-                                                [extport.EXT_INTERFACE_ID]['default'])
+        if result.get(extport.EXT_NODE_NAME) is None:
+            result[extport.EXT_NODE_NAME] = (extport.EXTENDED_ATTRIBUTES_2_0['ports']
+                                             [extport.EXT_NODE_NAME]['default'])
+        if result.get(extport.EXT_INTERFACE_NAME) is None:
+            result[extport.EXT_INTERFACE_NAME] = (extport.EXTENDED_ATTRIBUTES_2_0['ports']
+                                                  [extport.EXT_INTERFACE_NAME]['default'])
