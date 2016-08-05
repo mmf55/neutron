ca2b5727e826df5dc9283709de183ca810136ae1
mfernandes <manuelfernandes55@ua.pt>
2016-08-05 12:36:25 +0100

    ...

--- neutron/plugins/ml2/extnet/extnet_misc.py
+++ neutron/plugins/ml2/extnet/extnet_misc.py
@@ -349,7 +349,6 @@ class ExtNetControllerMixin(extnet_db_mixin.ExtNetworkDBMixin,
                     links_on_db = self._get_all_links_on_extsegment(context, extsegment.id, network_id)
                     if links_on_db:
                         link_on_db = links_on_db[0]
-                        LOG.debug(link_on_db.segmentation_id)
                         segmentation_id = link_on_db.segmentation_id
                         link_on_db.extports.append(context.session.query(models.ExtPort).filter_by(id=port_id).first())
                     else:
@@ -365,6 +364,7 @@ class ExtNetControllerMixin(extnet_db_mixin.ExtNetworkDBMixin,
                         segmentation_id = extlink_created.get('segmentation_id')
             i += 1
             if i == len(path):
+                LOG.debug(segmentation_id)
                 port['segmentation_id'] = segmentation_id
         return const.OK
 
