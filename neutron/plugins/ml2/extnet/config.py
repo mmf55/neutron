673e5ca418bf63d7bec612eceff8fe968d9d4196
mfernandes <manuelfernandes55@ua.pt>
2016-08-14 16:09:44 +0100

    ...

--- neutron/plugins/ml2/extnet/config.py
+++ neutron/plugins/ml2/extnet/config.py
@@ -4,33 +4,29 @@ netctrl_group = cfg.OptGroup(name='EXTNET_CONTROLLER',
                              title='Network controller default options.')
 
 netctrl_opts = [
-    cfg.DictOpt('network_controller_info',
-                default='ids_available: None',
-                required=False,
-                help='Network controller node name.'),
-
     cfg.StrOpt('name',
                default='OVS',
                required=False,
                help='Network controller name.'),
 
     cfg.StrOpt('ip_address',
-               default='192.168.2.2',
+               # default='192.168.2.2',
                required=False,
                help='Network controller IP address.'),
 
     cfg.StrOpt('netmask',
-               default='255.255.255.0',
+               # default='255.255.255.0',
                required=False,
                help='Network controller IP address.'),
 
     cfg.StrOpt('nexthop_ip',
-               default='192.168.2.1',
+               # default='192.168.2.1',
+               default='10.0.0.1',
                required=False,
                help='Network controller next hop device.'),
 
     cfg.StrOpt('ids_available',
-               default='',
+               default='11:20',
                required=False,
                help='Network controller next hop device.'),
 
@@ -38,12 +34,6 @@ netctrl_opts = [
                 default="q-agent-notifier: OVS,extnet_agent: ESW1;ESW2;ESW4",
                 required=False,
                 help='Device controller name.'),
-    cfg.StrOpt('network_mapper_namespace',
-               required=False,
-               help=''),
-    cfg.StrOpt('network_mapper_class',
-               required=False,
-               help=''),
 ]
 
 cfg.CONF.register_group(netctrl_group)
